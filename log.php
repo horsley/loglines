7a6e8eaf9bd87ba19f64bddeee9efd781ced6634
horsley <imhorsley@gmail.com>
2014-01-30 00:35:42 +0800

    末页检测和条目数调整

--- log.php
+++ log.php
@@ -33,17 +33,27 @@ function make_index() {
  * 分页读取
  * @param int $page_num
  * @param int $page_size
+ * @param bool $is_last_page 是否最后一页
  * @return bool
  */
-function paged_read_lines($page_num = 0, $page_size = 300) {
+function paged_read_lines($page_num = 0, $page_size = 300, &$is_last_page = true) {
     //利用索引定位
     $handle = @fopen(DATA_INDEX, 'r');
     if (!$handle) {return false;}
-    fseek($handle, - ($page_num + 1) * $page_size * 4 - 4, SEEK_END);
+    $index_stat = fstat($handle);
+    $total_lines = $index_stat['size'] / 4; //总行数 包含最后一行是空白行
+
+    fseek($handle, - ($page_num + 1) * $page_size * 4 - 4, SEEK_END); //如果页数太大肯定会seek到开头
+    if (ftell($handle) == 0) {
+        $is_last_page = true;
+        $page_size = ($total_lines - 1) % $page_size; //最后一页条目数量的特殊处理
+    } else {
+        $is_last_page = false;
+    }
+
     $pos = unpack('Loffset', fread($handle, 4));
     $pos = array_pop($pos);
     fclose($handle);
-    var_dump($pos);
     //读取单页数据
     $handle = @fopen(DATA_FILE, 'r');
     if ($handle) {
